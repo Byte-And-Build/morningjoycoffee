de2704b4dabf3fc63ea9ee6ff602cd9b0b4f5763
Byte-And-Build <byteandbuild@outlook.com>
2025-06-04 14:13:07 -0500

    First Deploy

--- backend/server.js
+++ backend/server.js
@@ -21,13 +21,13 @@ const corsOptions = {
   credentials: true, // Allow cookies if needed
 };
 
-const path = require("path");
+// const path = require("path");
 
-app.use(express.static(path.join(__dirname, "client")));
+// app.use(express.static(path.join(__dirname, "client")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "index.html"));
+// });
 
 
 app.use(cors(corsOptions));
